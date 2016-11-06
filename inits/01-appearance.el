4fe9c88c6e7cd830ad3794cd08b5f27ddd248ddd
Shigeru Fujiwara <sgr0502@gmail.com>
2016-11-07 00:16:56 +0900

    added web-mode

--- inits/01-appearance.el
+++ inits/01-appearance.el
@@ -4,3 +4,16 @@
 (setq inhibit-startup-message t)
 ;;(global-hl-line-mode 1)
 (column-number-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (queue))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
