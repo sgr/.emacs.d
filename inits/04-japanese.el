1d8277ca242a8f1b23a436b55c41a1b1300b77c9
Shigeru Fujiwara <sgr0502@gmail.com>
2017-01-05 14:02:42 +0900

    added mozc settings for Japanese Input (It's need to add 'emacs-mozc' and 'emacs-mozc-bin' on Ubuntu)

--- inits/04-japanese.el
+++ inits/04-japanese.el
@@ -1,2 +1,9 @@
-(set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
+(set-file-name-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+
+(set-language-environment "Japanese")
+
+(when (require 'mozc nil 'noerror)
+  (setq default-input-method "japanese-mozc"))
