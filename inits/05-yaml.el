f927877e1eadfbda7b2b2773126a0b23ffdaf110
Shigeru Fujiwara <shigeru.fujiwara@aoba-denshi.com>
2017-06-10 16:40:26 +0900

    added yaml-mode

---
+++ inits/05-yaml.el
@@ -0,0 +1,6 @@
+(el-get-bundle! yaml-mode
+  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+  (add-to-list 'auto-mode-alist '("\\.yml.example\\'" . yaml-mode))
+  (add-hook 'yaml-mode-hook
+            (lambda ()
+              (define-key yaml-mode-map "\C-m" 'newline-and-indent))))
