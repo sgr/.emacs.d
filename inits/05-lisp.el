62aada09a0323dbe55f0e97967937ecdf289cd92
Shigeru Fujiwara <shigeru.fujiwara@aoba-denshi.com>
2018-01-03 00:06:06 +0900

    added slime

--- inits/05-lisp.el
+++ inits/05-lisp.el
@@ -19,6 +19,33 @@
   (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
   (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode))
 
+(el-get-bundle! slime
+  (setq inferior-lisp-program "sbcl")
+  (setq slime-net-coding-system 'utf-8-unix)
+  (slime-setup '(slime-repl slime-fancy slime-banner slime-indentation)))
+
+(el-get-bundle! popwin
+    ;; Apropos
+  (push '("*slime-apropos*") popwin:special-display-config)
+  ;; Macroexpand
+  (push '("*slime-macroexpansion*") popwin:special-display-config)
+  ;; Help
+  (push '("*slime-description*") popwin:special-display-config)
+  ;; Compilation
+  (push '("*slime-compilation*" :noselect t) popwin:special-display-config)
+  ;; Cross-reference
+  (push '("*slime-xref*") popwin:special-display-config)
+  ;; Debugger
+  (push '(sldb-mode :stick t) popwin:special-display-config)
+  ;; REPL
+  (push '(slime-repl-mode) popwin:special-display-config)
+  ;; Connections
+  (push '(slime-connection-list-mode) popwin:special-display-config))
+
+(el-get-bundle! ac-slime
+  (add-hook 'slime-mode-hook 'set-up-slime-ac)
+  (add-hook 'slime-repl-mode-hook 'set-up-slime-ac))
+
 (when (and (<= 24 emacs-major-version) (<= 4 emacs-minor-version))
   ;; Clojure
   (el-get-bundle! clojure-mode
