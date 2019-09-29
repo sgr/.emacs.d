183fc6ec10c0dcc9bb6a4a1705c8e7e391b199ff
Shigeru Fujiwara <shigeru.fujiwara@aoba-denshi.com>
2019-09-30 03:55:16 +0900

    magit commented out

--- inits/03-packages.el
+++ inits/03-packages.el
@@ -3,8 +3,8 @@
 (el-get-bundle less-css-mode)
 (el-get-bundle ruby-mode)
 (el-get-bundle markdown-mode)
-(when (and (<= 24 emacs-major-version) (<= 4 emacs-minor-version))
-  (el-get-bundle magit))
+;; (when (and (<= 24 emacs-major-version) (<= 4 emacs-minor-version))
+;;   (el-get-bundle magit))
 (el-get-bundle dockerfile-mode
   (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode)))
 (el-get-bundle arduino-mode)
